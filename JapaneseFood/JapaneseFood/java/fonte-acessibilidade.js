35079894f9d0f11dc0c2d665d08540b468480a5b
Thiago-Bitencourte <48037919+Thiago-Bitencourte@users.noreply.github.com>
2019-04-04 11:37:20 -0300

    Add files via upload

---
+++ JapaneseFood/JapaneseFood/java/fonte-acessibilidade.js
@@ -0,0 +1,24 @@
+<script type="text/javascript" charset="utf-8">
+var $z = jQuery.noConflict();
+$z(document).ready(function(){
+$z("#aumentar-fonte").click(function () {
+var size = $z("#content p").css('font-size');
+
+size = size.replace('px', '');
+size = parseInt(size) + 2;
+
+$z("#content p").animate({'font-size' : size + 'px'});
+return false;
+});
+
+$z("#diminuir-fonte").click(function () {
+var size = $z("#content p").css('font-size');
+
+size = size.replace('px', '');
+size = parseInt(size) - 2;
+
+$z("#content p").animate({'font-size' : size + 'px'});
+return false;
+});
+});
+</script>
